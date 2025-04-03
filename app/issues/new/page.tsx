8b124d6e35e2bd38310f66b7709197601fc9ec12
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-04-03 21:09:09 +0330

    Handle errors in new issue form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { Controller, useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import SimpleMDE from "react-simplemde-editor";
+import { CiCircleAlert } from "react-icons/ci";
 
 interface Issue {
   title: string;
@@ -15,25 +16,43 @@ interface Issue {
 const NewIssuePage = () => {
   const { register, control, handleSubmit } = useForm<Issue>();
   const router = useRouter();
+  const [error, setError] = useState("");
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="New Issue" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Add Description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5">
+          <Callout.Icon>
+            <CiCircleAlert />
+          </Callout.Icon>
+          <Callout.Text>
+            You will need admin privileges to install and access this
+            application.
+          </Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error happened.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="New Issue" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Add Description" {...field} />
+          )}
+        />
 
-      <Button>Submit</Button>
-    </form>
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
