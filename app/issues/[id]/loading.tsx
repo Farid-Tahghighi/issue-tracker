ca24da46acabb7b9eacc12415d7cbe930efb3424
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-05-03 15:56:53 +0330

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Heading, Flex, Card, Box } from "@radix-ui/themes";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const IssueDetailLoadingPage = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <Box className="max-w-xl">
+      <Heading>
+        <Skeleton />
+      </Heading>
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
+};
 
-export default IssueDetailLoadingPage
+export default IssueDetailLoadingPage;
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,19 @@
+import Spinner from "@/app/components/Spinner";
+import { Box, Button } from "@radix-ui/themes";
 import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssueLoadingPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl space-y-2">
+      <Skeleton height="2rem" />
+      <Skeleton height="20rem" />
+      <Button>
+        <Spinner />
+      </Button>
+    </Box>
+  );
 };
 
 export default NewIssueLoadingPage;
