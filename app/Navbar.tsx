a3d771b86539d330b9e766881a1533550f393434
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-05-24 20:27:59 +0330

    Refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -15,12 +15,6 @@ import { usePathname } from "next/navigation";
 import { LiaBugSolid } from "react-icons/lia";
 
 const Navbar = () => {
-  const { status, data: session } = useSession();
-  const currentPath = usePathname();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
   return (
     <nav className="mb-4 px-5 py-5 border-b">
       <Container>
@@ -29,55 +23,71 @@ const Navbar = () => {
             <Link href="/">
               <LiaBugSolid />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={clsx("hover:text-zinc-800 transition-colors", {
-                      "text-zinc-800": currentPath === link.href,
-                      "text-zinc-500": currentPath !== link.href,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
           <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Box asChild className="cursor-pointer">
-                    <button style={{ all: "unset" }}>
-                      <Avatar
-                        src={session.user!.image!}
-                        fallback="?"
-                        size="2"
-                        radius="full"
-                        referrerPolicy="no-referrer"
-                      />
-                    </button>
-                  </Box>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text>{session.user?.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item asChild>
-                    <Link href={"/api/auth/signout"}>Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href={"/api/auth/signin"}>Login</Link>
-            )}
+            <AuthMenu />
           </Box>
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={clsx({
+              "nav-link": true,
+              "!text-zinc-800": currentPath === link.href,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthMenu = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Link className="nav-link" href={"/api/auth/signin"}>Login</Link>;
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Box asChild className="cursor-pointer">
+          <button style={{ all: "unset" }}>
+            <Avatar
+              src={session!.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              referrerPolicy="no-referrer"
+            />
+          </button>
+        </Box>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label>
+          <Text>{session!.user?.email}</Text>
+        </DropdownMenu.Label>
+        <DropdownMenu.Item asChild>
+          <Link href={"/api/auth/signout"}>Log out</Link>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 };
 
