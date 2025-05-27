a84060a67a55d6ac5cf0bf392862fa3b2126d898
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-05-27 12:58:23 +0330

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,26 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [assginees, setAssignees] = useState<User[]>([]);
+  useEffect(() => {
+    axios.get<User[]>("/api/users").then((users) => setAssignees(users.data));
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Farid Tahghigi</Select.Item>
+          {assginees.map((assignee) => (
+            <Select.Item key={assignee.id} value={assignee.id}>
+              {assignee.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
