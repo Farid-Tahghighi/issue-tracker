05cd1ffc01e581a56af6d8b5bf14598dc122eee7
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-05-29 10:03:43 +0330

    Enhance the API to assign issues to users

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import { IssueSchema } from "@/app/validationSchemas";
+import { patchIssueScheme } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,9 +7,18 @@ export async function PATCH(
   { params }: { params: { id: string } }
 ) {
   const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueScheme.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
+  const { title, description, assignedToUserId } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+    }
+  }
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -19,8 +28,9 @@ export async function PATCH(
       id: parseInt(params.id),
     },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
   return NextResponse.json(updatedIssue);
@@ -34,11 +44,11 @@ export async function DELETE(
     where: { id: parseInt(params.id) },
   });
 
-  if(!issue) {
-    return NextResponse.json({error: "Invalid issue"}, {status: 404});
+  if (!issue) {
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
   }
   await prisma.issue.delete({
-    where: {id: issue.id}
-  })
+    where: { id: issue.id },
+  });
   return NextResponse.json({});
 }
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -4,3 +4,22 @@ export const IssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
 });
+
+export const patchIssueScheme = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required.")
+    .max(255, "Title is too long.")
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65335, "Description is too long.")
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "At least one assignee needed.")
+    .max(255, "Too much assignees")
+    .optional()
+    .nullable(),
+});
