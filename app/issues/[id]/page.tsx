f10bfd09b598751fbe2595e599771dfd2a72a38e
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-06-01 13:00:41 +0330

    Optimize perforamamce using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,15 +6,18 @@ import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
 import { auth } from "@/auth";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 interface Props {
   params: { id: string };
 }
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
+
 const IssueDetailsPage = async ({ params }: Props) => {
   const session = await auth();
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   if (!issue) notFound();
 
   return (
@@ -36,9 +39,7 @@ const IssueDetailsPage = async ({ params }: Props) => {
 };
 
 export async function generateMetadata({ params }: Props) {
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+  const issue = await fetchIssue(parseInt(params.id));
   return {
     title: issue?.title,
     description: "Details of issue " + issue?.id,
