f627a6c8eedf264b094d0ba24e71dd611b28f722
Farid Tahghighi <fariidtahghighi@gmail.com>
2025-08-02 13:11:46 +0330

    Remove Redux because apparently it's useless in next.js

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import React from "react";
-import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./store/store";
 import QueryClientProvider from "./QueryClientProvider";
 import AuthProvider from "./auth/provider";
 import { Theme } from "@radix-ui/themes";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ReduxProvider store={store}>
       <QueryClientProvider>
         <AuthProvider>
           <Theme>{children}</Theme>
         </AuthProvider>
       </QueryClientProvider>
-    </ReduxProvider>
   );
 };
 
